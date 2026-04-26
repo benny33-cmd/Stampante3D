fd51c713e907a0f1dea1e5d4837339134112877d
benny33-cmd <broccolinainformatica@gmail.com>
2026-04-26 17:39:59 +0200

    Update and rename application.java to Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BookingService service = new BookingService();
+
+        Booking b1 = new Booking("Terra", "7:55-8:50", "Mario");
+        boolean ok = service.prenota(b1);
+
+        if (ok) {
+            System.out.println("Prenotazione effettuata!");
+        } else {
+            System.out.println("Orario già occupato!");
+        }
+    }
+}
