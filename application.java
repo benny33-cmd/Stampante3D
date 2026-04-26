7b7e6d8e36280ff52054de555e6d631b818c1fab
benny33-cmd <broccolinainformatica@gmail.com>
2026-04-26 17:27:02 +0200

    Create application.java

---
+++ application.java
@@ -0,0 +1,11 @@
+package com.scuola.stampante3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stampante3dApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Stampante3dApplication.class, args);
+    }
+}
